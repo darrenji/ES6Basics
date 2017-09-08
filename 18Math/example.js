af76dd635ee593e14dd06247a749c37f3a31ab2b
darren <764190362@qq.com>
2017-09-08 16:48:05 +0800

    Math Finished

---
+++ 18Math/example.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Darren on 2017/9/8.
+ */
+Math.trunc(1.99);
+Math.sign(5);
+Number.isInteger(5);
+let maxVal = Number.MAX_SAFE_INTEGER;
+let minVal = Number.MIN_SAFE_INTEGER;
+
+Number.isSafeInteger(maxVal);
+Number.parseInt('1234',10);
+Number.parseFloat('1.2');
+Number.isFinite(5);
+Number.isNaN(0/0);
